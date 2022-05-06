5bec05610ae997d9a72c202b6acba38bb5d751d1
Pau Egea <papau84@gmail.com>
2022-05-06 16:40:32 +0200

    assertions

---
+++ demos/src/04-assertions.test.js
@@ -0,0 +1,32 @@
+test('test obj', () => {
+  const data = { name: 'nico' };
+  data.lastname = 'molina';
+  expect(data).toEqual({ name: 'nico', lastname: 'molina' });
+});
+
+//
+test('null', () => {
+  const data = null;
+  expect(data).toBeNull();
+  expect(data).toBeDefined();
+  expect(data).not.toBeUndefined();
+});
+
+test('booleans', () => {
+  expect(true).toEqual(true);
+  expect(false).toEqual(false);
+
+  expect(0).toBeFalsy();
+  expect('').toBeFalsy();
+  expect(false).toBeFalsy();
+});
+
+test('string', () => {
+  expect('Christoph').toMatch(/stop/);
+});
+
+test('list / array', () => {
+  const numbers = [1, 2, 3, 4, 5];
+  expect(numbers).toContain(3);
+});
+//
