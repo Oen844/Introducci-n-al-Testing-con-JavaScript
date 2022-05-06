db93b991466a3022940b86964a8c8e9287f997c5
Pau Egea <papau84@gmail.com>
2022-05-06 22:22:07 +0200

    methodology

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,12 +2,16 @@ const Person = require('./06-person');
 
 describe('Test for Person', () => {
   let person;
+  //Arrange / Given
   beforeEach(() => {
     person = new Person('Juan', 80, 1.80);
   });
   test('shold return down', () => {
+    //Arrange / Given
     person.weight = 45;
+    //Act / When
     const imc = person.calcIMC();
+    //Assert / Then
     expect(imc).toBe('down');
   });
 
