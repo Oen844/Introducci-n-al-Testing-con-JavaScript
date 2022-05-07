f1293dadb7717dcdfc2387241c208551fe4a92d4
Pau Egea <papau84@gmail.com>
2022-05-07 22:48:26 +0200

    moking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,19 +1,34 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('Test for getBooks', () => {
     test('should return a list book', () => {
       // Arrange
       // Act
-      const books = service.getBooks();
+      const books = service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.lenght).toEqual();
+      expect(books.length).toEqual(1);
     });
   });
 });
